22dfbc67ebbacfd28629641c567421cd37683388
Bowei Liu <bowei_liu@ymail.com>
2018-05-26 21:57:30 -0700

    added tests for sync_sombol_to_local and get_historical_data_prices

--- src/main/python/analytic/data/tws_data_api_connector.py
+++ src/main/python/analytic/data/tws_data_api_connector.py
@@ -12,6 +12,18 @@ from analytic.utility import RAW_DATA_PATH
 pd.set_option('display.max_columns', None)
 pd.set_option('display.max_rows', None)
 
+__dtype_dict = {
+    "m_time": np.int32,
+    "m_volume": np.int64,
+    "m_count": np.int32,
+    "m_wap": np.float64,
+    "m_high": np.float64,
+    "m_low": np.float64,
+    "m_open": np.float64,
+    "m_close": np.float64,
+    "m_time_iso": np.str_,
+}
+
 
 class BarSize(Enum):
     """
@@ -26,6 +38,15 @@ class BarSize(Enum):
 
 def get_historical_data_prices(symbol: str, end_local_date_str: str, num_of_bars: int, bar_size: BarSize,
                                inside_rth: bool) -> DataFrame:
+    """
+
+    :param symbol:
+    :param end_local_date_str:
+    :param num_of_bars:
+    :param bar_size:
+    :param inside_rth:
+    :return: a pandas.DataFrame indexed by m_time_iso
+    """
     if not symbol:
         raise ValueError("symbol cannot be None or empty")
     if not end_local_date_str:
@@ -46,27 +67,15 @@ def get_historical_data_prices(symbol: str, end_local_date_str: str, num_of_bars
     }
 
     # sending get request and saving the response as response object
-    r = requests.get(url=url, params=params, timeout=60)
+    r = requests.get(url=url, params=params, timeout=20)
     # extracting data in json format
     # data = r.json()
-
-    dtype_dict = {
-        "m_time": np.int32,
-        "m_volume": np.int64,
-        "m_count": np.int32,
-        "m_wap": np.float64,
-        "m_high": np.float64,
-        "m_low": np.float64,
-        "m_open": np.float64,
-        "m_close": np.float64,
-        "m_time_iso": np.str_,
-    }
-    df = pd.read_json(r.content, orient="records", typ="frame", dtype=dtype_dict, date_unit="s")
+    df = pd.read_json(r.content, orient="records", typ="frame", dtype=__dtype_dict, date_unit="s")
     df["m_time_iso"] = pd.to_datetime(df["m_time_iso"], yearfirst=True)
     df.set_index("m_time_iso", inplace=True)
     df.drop("m_time", axis=1, inplace=True)
 
-    print(df)
+    # print(df.head(5))
     return df
 
 
@@ -78,14 +87,33 @@ def sync_sombol_to_local(symbol: str, bar_size: BarSize=BarSize.DAY_1, file_path
     :param file_path: target file path to hold data
     :return: true if new data is downloaded, false means file_path is already updated.
     """
-    if not symbol:
+    if bar_size != BarSize.DAY_1:
+        raise ValueError("bar_size only supports DAY_1")
+    if type(symbol) is not str or not symbol:
         raise ValueError("symbol cannot be None or emtpy")
-    symbol = symbol.capitalize()
-
+    symbol = symbol.upper()
+    current_timestamp = pd.Timestamp(ts_input=datetime.datetime.now())
     if not file_path:
         file_path = RAW_DATA_PATH + "/" + symbol + "-TWS-DATA.csv"
-    end_local_date_str = datetime.datetime.now().strftime(time_utils.IB_LOCAL_DATETIME_FORMAT)
-    print(end_local_date_str)
-    df = get_historical_data_prices(symbol, end_local_date_str, 272, bar_size, True)
-    df.to_csv(file_path)
+    num_of_days_needed = 272
+    orignal_df = None
+    try:
+        orignal_df = pd.read_csv(file_path, parse_dates=True, index_col="m_time_iso", dtype=__dtype_dict)
+        time_delta = current_timestamp - orignal_df.index[-1]
+        print("time_delta.days: {}".format(time_delta.days))
+        num_of_days_needed = time_delta.days
+    except FileNotFoundError:
+        print("no filepath : {}".format(file_path))
+
+    end_local_date_str = current_timestamp.strftime(time_utils.IB_LOCAL_DATETIME_FORMAT)
+    print("end_local_date_str: {}".format(end_local_date_str))
+    df = get_historical_data_prices(symbol, end_local_date_str, num_of_days_needed, bar_size, True)
+    if orignal_df is None:
+        df.to_csv(file_path)
+        return df
+    for ele in df.index:
+        orignal_df.loc[ele] = df.loc[ele]
+    orignal_df.to_csv(file_path)
+    return orignal_df
+
 
--- src/test/python/analytic/data/test_tws_data_api_connector.py
+++ src/test/python/analytic/data/test_tws_data_api_connector.py
@@ -1,6 +1,9 @@
 import unittest
 from analytic.data.tws_data_api_connector import BarSize
 from analytic.data import tws_data_api_connector
+from analytic.utility import RAW_DATA_PATH
+import os
+import pandas as pd
 
 
 class TestTwsDataApiConnector(unittest.TestCase):
@@ -10,9 +13,34 @@ class TestTwsDataApiConnector(unittest.TestCase):
         end_local_date_str = "20180524 16:00:00"
         res_data = \
             tws_data_api_connector.get_historical_data_prices(symbol, end_local_date_str, 5, BarSize.DAY_1, True)
-        # TODO(Bowei) assertions pending
+        last_row = res_data.iloc[-1]
+        # print(last_row)
+        self.assertEqual(last_row["m_close"], 247.69)
+        self.assertEqual(last_row["m_count"], 35770.00)
+        self.assertEqual(last_row["m_high"], 249.40)
+        self.assertEqual(last_row["m_low"], 245.24)
+        self.assertEqual(last_row["m_volume"], 77090)
+        self.assertEqual(last_row["m_wap"], 247.63)
 
     def test_sync_sombol_to_local(self):
         symbol = "NVDA"
+        target_file = os.path.join(RAW_DATA_PATH, "{}-TWS-DATA.csv".format(symbol))
         tws_data_api_connector.sync_sombol_to_local(symbol)
-        # TODO(Bowei) assertions pending
+        df = pd.read_csv(target_file, parse_dates=True, index_col="m_time_iso")
+        self.assertTrue("m_time_iso" == df.index.name)
+        self.assertTrue("m_close" in df.columns)
+        self.assertTrue("m_count" in df.columns)
+        self.assertTrue("m_high" in df.columns)
+        self.assertTrue("m_low" in df.columns)
+        self.assertTrue("m_open" in df.columns)
+        self.assertTrue("m_volume" in df.columns)
+        self.assertTrue("m_wap" in df.columns)
+        target_row = df.loc[pd.Timestamp("2018-05-25 09:00:00")]
+        # print(target_row)
+        self.assertEqual(target_row["m_close"], 249.28)
+        self.assertEqual(target_row["m_count"], 23654)
+        self.assertEqual(target_row["m_high"], 249.94)
+        self.assertEqual(target_row["m_low"], 246.76)
+        self.assertEqual(target_row["m_open"], 248.20)
+        self.assertEqual(target_row["m_volume"], 50867)
+        self.assertAlmostEqual(target_row["m_wap"], 248.876, 2)
